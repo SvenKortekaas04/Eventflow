cebfaefa5704c0f96bc0d4b0aad5323f4593f28a
SvenKortekaas04 <50526577+SvenKortekaas04@users.noreply.github.com>
2021-01-20 10:42:46 +0100

    Move the version to a separate file

--- eventflow/__init__.py
+++ eventflow/__init__.py
@@ -2,14 +2,10 @@
 Eventflow provides functionalities for event-based applications. It's lightweight 
 and built with simplicity and elegance in mind. Therefore, Eventflow should be 
 easy to learn. It is written in pure Python and has no external dependencies.
+
+.. codeauthor:: Sven Kortekaas
 """
 
 from .bus import EventBus, Event
 
 __all__ = ("EventBus", "Event", "__version__")
-
-MAJOR_VERSION = 0
-MINOR_VERSION = 1
-PATCH_VERSION = 0
-
-__version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"

d182f18e1237de9e2522bf8e70a2607ecdb55193
Sven Kortekaas <svenkortekaas04@gmail.com>
2021-03-30 20:27:54 +0200

    Update text

--- eventflow/__init__.py
+++ eventflow/__init__.py
@@ -1,7 +1,7 @@
 """
 Eventflow provides functionalities for event-based applications. It's lightweight 
 and built with simplicity and elegance in mind. Therefore, Eventflow should be 
-easy to learn. It is written in pure Python and has no external dependencies.
+easy to learn.
 
 .. codeauthor:: Sven Kortekaas
 """
